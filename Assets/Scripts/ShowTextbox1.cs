bdb499b45390d6d65bd483b7b3e535b30caf2c1d
malik1097 <2.10.malik.arslan@gmail.com>
2024-01-23 23:10:58 +0100

    Add text prefab

---
+++ Assets/Scripts/ShowTextbox1.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTextbox : MonoBehaviour
+{
+    GameObject textbox;
+    PlayQuickSound sound;
+    AudioSource audioSource =  null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textbox = GameObject.Find("Canvas1");
+        textbox.SetActive(false);
+        sound = GameObject.Find("AudioSample").GetComponent<PlayQuickSound>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player entered trigger");
+            textbox.SetActive(true);
+            audioSource.Play();
+        }
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player left trigger");
+            StartCoroutine(DeactivateAfterDelay());
+        }
+    }
+    IEnumerator DeactivateAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+        textbox.SetActive(false);
+    }
+}
